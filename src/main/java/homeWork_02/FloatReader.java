15a3e9b0b118c7dc40a69edd6a4a237916928928
Adalberto777 <albert.tliashev@gmail.com>
2023-04-26 01:31:13 +0600

    floatReader

---
+++ src/main/java/homeWork_02/FloatReader.java
@@ -0,0 +1,29 @@
+package homeWork_02;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FloatReader {
+    /**
+     *  method that asks the user to enter a floating point value
+     */
+    public static void floatReader(){
+
+        Scanner scanner = new Scanner(System.in);
+        float value = 0;
+        boolean validInput = false;
+
+        do {
+            try {
+                System.out.print("Enter a floating point value: ");
+                value = scanner.nextFloat();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Try again. you must enter a floating point value");
+                scanner.nextLine();
+            }
+        } while (!validInput);
+
+        System.out.println("You enter: " + value);
+    }
+}
---
+++ src/main/java/homeWork_02/Main.java
@@ -0,0 +1,7 @@
+package homeWork_02;
+
+public class Main {
+    public static void main(String[] args) {
+        FloatReader.floatReader();
+    }
+}
